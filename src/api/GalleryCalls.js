29af2bed962e9169000295c04841282f7b4949a6
DirectlyMe <hansenja2011@gmail.com>
2018-09-08 14:32:06 -0600

    Testing new IP's

--- src/api/GalleryCalls.js
+++ src/api/GalleryCalls.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function getGalleryItems() {
 	return new Promise((resolve, reject) => {
 		axios
-			.get('http://localhost:8080/gallery/getGallery')
+			.get('http://192.168.1.120:8080/gallery/getGallery')
 			.then(response => resolve(response.data))
 			.catch(err => reject(err))
 	})
@@ -12,7 +12,7 @@ export function getGalleryItems() {
 export function getSingleItem(itemId) {
 	return new Promise((resolve, reject) => {
 		axios
-			.get(`http://localhost:8080/gallery/getGalleryItem/${itemId}`)
+			.get(`http://192.168.1.120:8080/gallery/getGalleryItem/${itemId}`)
 			.then(response => resolve(response.data))
 			.catch(err => reject(err))
 	})
